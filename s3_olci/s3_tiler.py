0d9d44e1f3566529a27c1ea04b0181a1b3343aee
Jose Gomez-Dans <jgomezdans@gmail.com>
2020-02-13 16:40:37 +0000

    VRT creation WiP

--- s3_olci/s3_tiler.py
+++ s3_olci/s3_tiler.py
@@ -9,6 +9,34 @@ import shapely.wkt
 from shapely.geometry import shape, GeometryCollection
 
 
+def create_vrts(fname, xsize, ysize):
+    vrt = f"""<VRTDataset rasterXSize="{xsize}" rasterYSize="{ysize}">
+          <SRS>GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9108"]],AUTHORITY["EPSG","4326"]]</SRS>
+      <VRTRasterBand dataType="Float32" band="1">
+        <SimpleSource>
+          <SourceFilename relativeToVRT="1">{fname}</SourceFilename>
+          <SourceBand>1</SourceBand>
+          <SourceProperties RasterXSize="{xsize}" RasterYSize="{ysize}" DataType="Float32" BlockXSize="256" BlockYSize="256" />
+          <SrcRect xOff="0" yOff="0" xSize="{xsize}" ySize="{ysize}" />
+          <DstRect xOff="0" yOff="0" xSize="{xsize}" ySize="{ysize}" />
+        </SimpleSource>
+      </VRTRasterBand>
+    </VRTDataset>"""
+
+    def convert_geoloc_tiff(data_dir):
+        nc_file = data_dir / "geolocation.nc"
+        for dimension in ["lat", "lon"]:
+            fname = data_dir/f"{dimension}.tif"
+            g = gdal.Warp( fname.as_posix(),
+                        f"'NETCDF:{nc_file}:{dimension}'",
+                        format="GeoTIFF",
+                        output_type=gdal.GDT_Float32)
+            xsize = g.RasterXSize
+            ysize = g.RasterYSize
+            vrt = create_vrts(f"{dimension}.tif", xsize, ysize)
+            fname.write_text(vrt)
+
+
 def read_s3syn_polyfile(s3_granule):
     """Reads the S3 WKT polygon. useful for figuring stuff out"""
     s3_granule = Path(s3_granule)
@@ -36,10 +64,13 @@ def get_swaths_overlap_tile(swaths, swath_geoms, tile_geom):
 
 
 if __name__ == "__main__":
-    tile_geom = get_modis_tile_extent("h18v04", "./modis-tiles.geojson" )
+    h = 18
+    v = 2
+    # h18v02 seems to be present
+    tile_geom = get_modis_tile_extent(f"h{h:02}v{v:02}", "./modis-tiles.geojson" )
     granules, swaths = find_swaths(300, 2019,  "/home/ucfajlg/temp/S3_BRDF/")
-    print(granules)
-    print(swaths)
-    print(get_swaths_overlap_tile(granules, swaths, tile_geom))
+    overlaps = get_swaths_overlap_tile(granules, swaths, tile_geom)
+    if overlaps:
+        print(f"h{h:02}v{v:02} => ", overlaps)
     #print(read_s3syn_polyfile(
     #    "/home/ucfajlg/temp/S3_BRDF/2019/300/S3_SYN_20191027T234335_20191027T234527_20191029T102608_0111_051_016_1800"))
